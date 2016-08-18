8941913b1f8306641c28053553786144e18a9597
Oliver Hoffmann <oliverh855@gmail.com>
2016-08-18 18:00:55 +0200

    Add skeleton project

---
+++ src/updater.ts
@@ -0,0 +1,3 @@
+export async function update () {
+  return await fetch('https://secure-software-engineering.github.io/rose/example/base.json')
+}
---
+++ test/updaterSpec.ts
@@ -0,0 +1,26 @@
+import {expect} from 'chai'
+import * as sinon from 'sinon'
+
+import * as fetchMock from 'fetch-mock'
+import {update} from '../src/updater'
+
+let server
+
+describe('test updater', () => {
+  before(() => {
+    fetchMock.mock('https://secure-software-engineering.github.io/rose/example/base.json', "HELLO WORLD", {method: 'GET'})
+  })
+
+  after(() => {
+    fetchMock.restore();
+  })
+
+  it('should work', () => {
+    expect(2).to.be.equal(2)
+  })
+
+  it('out should request', async () => {
+    const res = await update()
+    expect(await res.text()).to.be.equal("HELLO WORLD")
+  })
+})
